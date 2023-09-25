c0a14028710d5e66a5e0a735a4d7a1ce86b49b4c
Nicolas Aguilera García <niagar25@gmail.com>
2023-09-25 14:54:27 -0500

    [ADD] experiments

--- main.py
+++ main.py
@@ -1,41 +1,24 @@
-import random
+from models import GaltonBoard, Experiment, shwoHistogram
 
-def galton_board(rows, cols):
-    last_row = [[] for _ in range(cols)]
-
-    
-    def drop_ball(start_col):
-        grid = [[' ' for _ in range(cols)] for _ in range(rows)]
-        
-        row = 0
-        col = start_col
-
-        while row < rows:
-            grid[row][col] = 'o'
-            direction = random.choice([-1, 1])
-
-            # if not (col + direction >= 0):
-            #     direction += 1
-            
-            # if not (col + direction < cols):
-            #     direction += -1
-            
-            if col + direction >= 0 and col + direction < cols:
-                col += direction
-                
-            row += 1
-
-        last_row[col].append('o')
-
-    number_of_balls = 1500
-    for _ in range(number_of_balls):
-        initial_col = random.randint(0, cols - 1)
-        drop_ball(19)
-    
-    for row in last_row:
-        print(''.join(row))
-    
 if __name__ == "__main__":
-    rows = 50
-    cols = 20
-    galton_board(rows, cols)
+    num_slots = 30
+    num_rows = 40
+    num_balls = 1000
+    
+    num_trials = num_balls * 2
+    num_experiments = 1
+
+    experiments: list[Experiment] = []
+
+    for _ in range(num_experiments):
+        galton_board = GaltonBoard(num_slots, num_rows, num_balls)
+        galton_board.setStartSlot(10)
+
+        experiment = Experiment()
+        experiment.run(galton_board, num_trials)
+
+        experiments.append(experiment)
+
+    print(len(experiments[0].boards_slots))
+
+    shwoHistogram(experiments[0].getAverageSlots())
